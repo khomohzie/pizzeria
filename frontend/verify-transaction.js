1a69d8ca1a39712ae7f1a41974e8376a9016e2d8
Nnamdi Samuel-Johnson <nnamdisj@gmail.com>
2022-08-24 15:36:25 +1100

    verify transaction

---
+++ frontend/verify-transaction.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+    const verifyTransaction = async function () {
+        let token = localStorage.getItem('token')
+        if (!token) return
+
+        let request = await fetch('https://pizzeria-oop.herokuapp.com/api/user/me', {
+            method: 'get',
+            headers: {
+                'authorization': `Bearer ${token}`
+            }
+        }).then(res => res.json()).catch(err => { return })
+
+        if (!request.success) {
+            localStorage.removeItem('token')
+            return alert('User must be logged in')
+        }
+        console.log(request.data)
+
+        let reference = localStorage.getItem('paystack');
+
+        const verifyRequest = await fetch(`localhost:4000/api/order/paystack/verify-transaction?reference=${reference}`, {
+            method: 'get',
+            headers: {
+                'authorization': `Bearer ${token}`
+            }
+        }).then(res => res.json()).catch(err => { console.log(err) })
+
+        if (verifyRequest.success) {
+           const messageElement = document.getElementById("verified-message");
+
+           messageElement.textContent = verifyRequest.message;
+        }
+    }
+}
