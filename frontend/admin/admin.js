60fa2e54280f91932fbc98afae77797a69a11403
Daniel Komolafe <komozy2000@gmail.com>
2022-08-25 11:30:48 +0100

    Admin can get all users and orders

--- frontend/admin/admin.js
+++ frontend/admin/admin.js
@@ -172,3 +172,112 @@ async function getUsers() {
 }
 
 getUsers();
+
+const orderscont = document.getElementById("orders");
+const pizzacont = document.getElementById("pizzas");
+const usercont = document.getElementById("users");
+
+const pizzaDiv = document.getElementById("pizzaform");
+const orderDiv = document.getElementById("orderDiv");
+const userDiv = document.getElementById("userDiv");
+
+orderscont.addEventListener("click", function () {
+	pizzacont.classList.remove(["current"]);
+	usercont.classList.remove(["current"]);
+	orderscont.classList.add(["current"]);
+
+	pizzaDiv.style.display = "none";
+	orderDiv.style.display = "block";
+
+	fetchOrders();
+});
+
+async function fetchOrders() {
+	let token = localStorage.getItem("token");
+	if (!token) window.location.assign("/");
+
+	await fetch("https://pizzeria-oop.herokuapp.com/api/order/all", {
+		method: "get",
+		headers: {
+			authorization: `Bearer ${token}`,
+		},
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data.data)
+
+			if (data.data.length > 0) {
+				document.getElementById("no-order-h").style.display = "none"
+			}
+
+			for (let i = 0; i < data.data.length; i++) {
+				let eachOrder = document.createElement("div");
+
+				eachOrder.style.background = "#89CFF0";
+				eachOrder.style.marginBottom = "16px";
+				eachOrder.style.padding = "12px";
+
+				eachOrder.innerHTML = `
+				<p>Name of purchaser: ${data.data[i].user.fullname}</p>
+				<p>Email of purchaser: ${data.data[i].user.email}</p>
+				<p>Contact of purchaser: ${data.data[i].user.contact}</p>
+				<p>Pizza ordered: ${data.data[i].pizza ? data.data[i].pizza.name : "Pizza"}</p>
+				<p>Amount: ${data.data[i].amount}</p>
+				`
+
+				orderDiv.append(eachOrder);
+			}
+		})
+		.catch((err) => console.log(err));
+}
+
+usercont.addEventListener("click", function () {
+	orderscont.classList.remove(["current"]);
+	pizzacont.classList.remove(["current"]);
+	usercont.classList.add(["current"]);
+
+	pizzaDiv.style.display = "none";
+	orderDiv.style.display = "none";
+	userDiv.style.display = "block";
+
+	fetchUsers();
+});
+
+async function fetchUsers() {
+	let token = localStorage.getItem("token");
+	if (!token) window.location.assign("/");
+
+	await fetch("https://pizzeria-oop.herokuapp.com/api/admin/users", {
+		method: "get",
+		headers: {
+			authorization: `Bearer ${token}`,
+		},
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data.data)
+
+			if (data.data.length > 0) {
+				document.getElementById("no-user-h").style.display = "none"
+			}
+
+			for (let i = 0; i < data.data.length; i++) {
+				let eachUser = document.createElement("div");
+
+				eachUser.style.background = "#89CFF0";
+				eachUser.style.marginBottom = "16px";
+				eachUser.style.padding = "12px";
+
+				eachUser.innerHTML = `
+				<p>Full name: ${data.data[i].fullname}</p>
+				<p>Email: ${data.data[i].email}</p>
+				<p>Username: ${data.data[i].username}</p>
+				<p>Contact: ${data.data[i].contact || "Admin has no contact"}</p>
+				<p>Role: ${data.data[i].role}</p>
+				`
+
+				userDiv.append(eachUser);
+			}
+		})
+		.catch((err) => console.log(err));
+}
